8756df72d9203f43ffb902131fddb030625c50e6
Arkadii Shvartcman <a.shvartcman@gmail.com>
2024-06-11 16:23:10 +0200

    nuxt example

---
+++ nuxt-example/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Parsed Cookies</h1>
+    <pre>{{JSON.stringify(cookie, null, 2)}}</pre>
+    <Button />
+  </div>
+</template>
+
+<script setup>
+
+import { useCookie } from 'nuxt/app'
+import { getUser } from '@tawasal/node'
+import Button from "~/components/button.vue";
+let cookie
+try {
+   cookie = useCookie("tawasal", { decode: getUser})
+} catch (e) {
+  console.log(e)
+}
+
+</script>
---
+++ nuxt-example/components/button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="haptic">
+    haptic
+  </button>
+</template>
+<script setup>
+import { useTawasalSDK } from '@tawasal/vue'
+
+const handlers = useTawasalSDK()
+const haptic = handlers.haptic;
+console.log(handlers)
+
+</script>
